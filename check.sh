5c4f61b8b6a3883a782116cb7df392a9725e986f
Tadeu Banzato <10622335+tadeubanzato@users.noreply.github.com>
2021-04-16 16:23:05 -0400

    update all

--- check.sh
+++ check.sh
@@ -5,6 +5,6 @@ if [ -f "$CHECK" ]; then
     echo "$CHECK exists."
 else
     echo "$CHECK does not exist."
-    cd /home/pi/telegrambot/factcheck
-    screen -dm -S CHECK python3 /home/pi/telegrambot/factcheck/fact_check.py
+    cd /home/pi/telegrambot/fact_check_brasil
+    screen -dm -S CHECK python3 /home/pi/telegrambot/fact_check_brasil/fact_check.py
 fi
